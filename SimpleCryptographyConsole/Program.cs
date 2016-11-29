5ca2578dd61d2f4c2fe5a2edd08b879e2fd4b2eb
David Allan <profissional@david-allan.com.br>
2016-11-29 11:54:18 -0200

    Splited Main method on two parts to process

--- SimpleCryptographyConsole/Program.cs
+++ SimpleCryptographyConsole/Program.cs
@@ -6,19 +6,28 @@ namespace SimpleCryptographyConsole
     {
         static void Main(string[] args)
         {
-            string passPhrase = getUserContent("Please enter a passphrase:");
+            string encryptedString = ProcessEncrytedPart();
+            ProcessDecryptesPart(encryptedString);
+        }
 
-            StringCipherConsumer cipher = new StringCipherConsumer(passPhrase);
-
-            string userText = getUserContent("Please enter your text:");
-
-            string encryptedString = cipher.Encrypt(userText);
-
-            ShowContent("your encrypted text is:" + encryptedString);
+        private static void ProcessDecryptesPart(string encryptedString)
+        {
+            var cipher = new StringCipherConsumer(getUserContent("Please enter a passphrase:"));
 
             ShowContent("Now lets decrypt this:" + encryptedString);
 
             ShowContent("So the decrypted value is:" + cipher.Decrypt(encryptedString));
+        }
+
+        private static string ProcessEncrytedPart()
+        {
+            var cipher = new StringCipherConsumer(getUserContent("Please enter a passphrase:"));
+            string userText = getUserContent("Please enter your text:");
+
+            var encryptedString = cipher.Encrypt(userText);
+            ShowContent("your encrypted text is:" + encryptedString);
+
+            return encryptedString;
         }
 
         private static string getUserContent(string message)
