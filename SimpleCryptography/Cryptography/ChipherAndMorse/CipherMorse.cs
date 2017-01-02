7c1b96840fda3849f05903a59651218639e296fb
David Allan <profissional@david-allan.com.br>
2017-01-02 19:30:43 -0200

    Added Morse Code to Cipher (CipherMorse)

---
+++ SimpleCryptography/Cryptography/ChipherAndMorse/CipherMorse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Morse = MorseCode.communication.MorseCode;
+using MorseCode.protocol;
+
+namespace SimpleCryptography.Cryptography.ChipherAndMorse
+{
+    public sealed class CipherMorse
+    {
+        public CipherMorse()
+        {
+
+        }
+
+        public StringCipher Cipher()
+        {
+            return new StringCipher();
+        }
+
+        public Morse MorseCode()
+        {
+            return new Morse(new AmericanMorse());
+        }
+    }
+}
