ef0e3f70769f7ad8913053b09f78e291b5a5e6ce
David Allan <profissional@david-allan.com.br>
2017-01-18 19:47:47 -0200

    Added Primitive Cipher (Rot13) Mr. Robot

---
+++ SimpleCryptography/PrimitiveCipher/Rot13.cs
@@ -0,0 +1,40 @@
+﻿namespace SimpleCryptography.PrimitiveCipher
+{
+    public class Rot13
+    {
+
+        public string Transform(string value)
+        {
+            char[] array = value.ToCharArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+                int number = (int)array[i];
+
+                if (number >= 'a' && number <= 'z')
+                {
+                    if (number > 'm')
+                    {
+                        number -= 13;
+                    }
+                    else
+                    {
+                        number += 13;
+                    }
+                }
+                else if (number >= 'A' && number <= 'Z')
+                {
+                    if (number > 'M')
+                    {
+                        number -= 13;
+                    }
+                    else
+                    {
+                        number += 13;
+                    }
+                }
+                array[i] = (char)number;
+            }
+            return new string(array);
+        }
+    }
+}
---
+++ SimpleCryptography/UnitTest_SimpleCryptography/PrimitiveCipher/Rot13Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using SimpleCryptography.PrimitiveCipher;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace UnitTest_SimpleCryptography.PrimitiveCipher
+{
+    [TestClass]
+    public class Rot13Test
+    {
+        [TestMethod]
+        public void canRotTo13()
+        {
+            var rot13 = new Rot13();
+            var text = "David";
+            var rot = rot13.Transform(text);
+
+            Assert.AreNotEqual(text, rot);
+        }
+
+        [TestMethod]
+        public void canRotTo13AndRecoverValue()
+        {
+            var rot13 = new Rot13();
+            var text = "David";
+            var rot = rot13.Transform(text);
+            var originalText = rot13.Transform(rot);
+
+            Assert.AreEqual(text, originalText);
+        }
+    }
+}
