c615b8b84b09770133865b0a83a2ec42192d6d05
David Allan <profissional@david-allan.com.br>
2017-01-25 10:06:38 -0200

    Added RotNTest

---
+++ SimpleCryptography/UnitTest_SimpleCryptography/PrimitiveCipher/RotNTest.cs
@@ -0,0 +1,32 @@
+﻿using SimpleCryptography.PrimitiveCipher;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest_SimpleCryptography.PrimitiveCipher
+{
+    [TestClass]
+    public class RotNTest
+    {
+        [TestMethod]
+        public void canRotToN()
+        {
+            var N = 12;
+            var rot13 = new RotN(N);
+            var text = "David";
+            var rot = rot13.Transform(text);
+
+            Assert.AreNotEqual(text, rot);
+        }
+
+        [TestMethod]
+        public void canRotToNAndRecoverValue()
+        {
+            var N = 15;
+            var rot13 = new RotN(N);
+            var text = "David";
+            var rot = rot13.Transform(text);
+            var originalText = rot13.Transform(rot);
+
+            Assert.AreEqual(text, originalText);
+        }
+    }
+}
